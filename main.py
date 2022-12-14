517c5fbd089bc2f0e94c1eb53665330f4bb66282
Noe Tipo <Noe.Tipo@adra.local>
2022-12-14 16:39:15 -0500

    pdf

--- main.py
+++ main.py
@@ -17,6 +17,24 @@ def crear_persona():
     personas.append(persona)
 
 
+lista_personas:list=[{"dni":"47259697","nombres":"Noe Wilber","apellidos":"Tipo Mamani","direccion":"UPeU","telefono":"997124032"},
+{"dni":"47259698","nombres":"juan","apellidos":" Mamani","direccion":"UPeU","telefono":"997124032"},
+{"dni":"47259699","nombres":"pedro","apellidos":"Tipo","direccion":"UPeU","telefono":"997124032"},
+{"dni":"47259610","nombres":"Adres","apellidos":"Tipo Mamani","direccion":"UPeU","telefono":"997124032"}]
+
+lista_productos:list=[{"codigo":"01","nombre":"Pan","precio":1.20,"marca":"Union"},
+{"codigo":"02","nombre":"Agua","precio":1.00,"marca":"Union"},
+{"codigo":"03","nombre":"Paneton","precio":35.00,"marca":"Union"},
+{"codigo":"04","nombre":"caramelo","precio":0.50,"marca":"Union"}]
+
+def cargar_datos():
+    for persona in lista_personas:
+        persona:Persona=Persona(persona["dni"],persona["nombres"],persona["apellidos"],persona["direccion"],persona["telefono"])
+        personas.append(persona)
+    for producto in lista_productos:
+        producto:Producto=Producto(producto["codigo"],producto["nombre"],producto["precio"],producto["marca"])
+        productos.append(producto)
+
 def listar_personas():
     for persona in personas:
         Persona.convertir_a_string(persona)
@@ -122,6 +140,27 @@ def buscar_factura():
 
 
 def main():
+    """from reportlab.lib.pagesizes import letter
+    from reportlab.pdfgen import canvas
+
+    canvas = canvas.Canvas("form.pdf", pagesize=letter)
+    canvas.setLineWidth(.3)
+    canvas.setFont('Helvetica', 12)
+
+    canvas.drawString(30,750,'OFFICIAL COMMUNIQUE')
+    canvas.drawString(30,735,'OF ACME INDUSTRIES')
+    canvas.drawString(500,750,"12/12/2010")
+    canvas.line(480,747,580,747)
+
+    canvas.drawString(275,725,'AMOUNT OWED:')
+    canvas.drawString(500,725,"$1,000.00")
+    canvas.line(378,723,580,723)
+
+    canvas.drawString(30,703,'RECEIVED BY:')
+    canvas.line(120,700,580,700)
+    canvas.drawString(120,703,"JOHN DOE")
+
+    canvas.save()"""
     continuar: bool = True
     while continuar:
 
